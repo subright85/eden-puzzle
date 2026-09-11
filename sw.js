f89e13dc87935a64d58ddb6feff9b828b506f203
subright85 <subright@gmail.com>
2026-09-11 15:50:43 -0700

    Add header mode toggle and refined puzzle brand mark

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'eden-v51';
+const C = 'eden-v52';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C)
     .then(c => c.addAll(['/', '/manifest.webmanifest', '/icon-180.png', '/icon-512.png', '/assets/title.jpg', '/assets/ui/painted/play-v1.png', '/assets/ui/painted/panel-v1.png', '/assets/ui/painted/tray-v1.png',
